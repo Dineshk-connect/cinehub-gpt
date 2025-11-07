7d794eccbd2c36758f3f6db9dce856f2a631df09
Dineshk-connect <dinesh.k.connect@gmail.com>
2025-11-07 21:21:44 +0530

    Sign In/ Sihn Up form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
-import './App.css';
+
+import Body from './components/Body';
 
 function App() {
   return (
-    <div className="flex items-center justify-center h-screen bg-gray-900">
-      <h1 className="text-5xl font-bold text-white">
-        Tailwind v4 + CRA Works 🎉
-      </h1>
-    </div>
+    <Body/>
   );
 }
 
